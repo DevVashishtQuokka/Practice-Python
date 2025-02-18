e9dbcf2fbe474551cf9babfa26fddde6119e0521
DevVashishtQuokka <devraj.sharma@quokkalabs.com>
2025-02-18 18:18:58 +0530

    Update task6a.py

--- task6a.py
+++ task6a.py
@@ -1,21 +1,19 @@
-def hourglass(n):
-    for i in range(n):
-        for j in range(2 * n - 1):
-            if j < i or j >= 2 * n - 1 - i:
-                print(" ", end=" ")
-            else:
-                if i == 0 or j == i or j == 2 * n - 2 - i:
-                    print("*", end=" ")
-                else:
-                    print(" ", end=" ")
-        print()
-    for i in range(n - 2, -1, -1):
-        for j in range(2 * n - 1):
-            if j < i or j >= 2 * n - 1 - i:
-                print(" ", end=" ")
-            else:
-                if i == 0 or j == i or j == 2 * n - 2 - i:
-                    print("*", end=" ")
-                else:
-                    print(" ", end=" ")
-        print()
+def hourglass(num):
+    l1 = []
+    outer = 0
+    inner = (2 * num)  - 5
+    
+    for i in range(num, 0, -1):
+        if i == num or i == 1:
+            l1.append(' ' * outer + '*' * ((2 * i) - 1))
+            outer += 1
+        else:
+            l1.append(' ' * outer + '*' + ' ' * inner + '*')
+            outer += 1
+            inner -= 2
+    for i in l1:
+        print(i)
+    for i in reversed(l1):
+        print(i)
+
+hourglass(6)
